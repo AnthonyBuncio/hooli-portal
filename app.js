56ce55515859e2e4e3e8b7123c50d19157dadb56
AnthonyBuncio <anthonyb.ft@gmail.com>
2019-01-23 17:03:07 -0600

    added the box

--- app.js
+++ app.js
@@ -7,30 +7,39 @@ const passport = require('passport');
 
 const app = express();
 
-//Passport config
+// Use static files in ejs
+app.use(express.static('public'));
+
+// Passport config
 require('./config/passport')(passport);
 
 // DB connection
 const db = require('./config/keys').MongoURI;
 
 // Connect to Mongo
-mongoose.connect(db, { useNewUrlParser: true })
+mongoose
+    .connect(
+        db,
+        {useNewUrlParser: true}
+    )
     .then(() => console.log('MongoDB Connected..'))
-    .catch(err => console.log(err))
+    .catch(err => console.log(err));
 
 // EJS
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
 // Bodyparser
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 
 // Express session middlware
-app.use(session({
-    secret: 'secret',
-    resave: true,
-    saveUninitialized: true
-}));
+app.use(
+    session({
+        secret: 'secret',
+        resave: true,
+        saveUninitialized: true
+    })
+);
 
 // Passport middleware
 app.use(passport.initialize());
@@ -45,11 +54,11 @@ app.use((req, res, next) => {
     res.locals.error_msg = req.flash('error_msg');
     res.locals.error = req.flash('error');
     next();
-})
+});
 
 // Routes
-app.use('/', require('./routes/index'))
-app.use('/users', require('./routes/users'))
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
 
 const PORT = process.env.PORT || 5000;
 
